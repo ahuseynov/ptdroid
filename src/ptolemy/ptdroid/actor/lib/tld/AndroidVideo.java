458e900d4f1736504536757590f8d389b4c5cf48
Jack Chen <jfchen100x@gmail.com>
2011-12-01 11:10:50 -0500

    Added bounding box for AndroidVideo

--- src/ptolemy/ptdroid/actor/lib/tld/AndroidVideo.java
+++ src/ptolemy/ptdroid/actor/lib/tld/AndroidVideo.java
@@ -12,22 +12,73 @@ import ptolemy.kernel.util.IllegalActionException;
 import ptserver.actor.lib.tld.Video;
 import ptserver.actor.lib.tld.VideoInterface;
 import ptserver.data.ByteArrayToken;
+import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.ImageFormat;
+import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.YuvImage;
+import android.graphics.Paint.Style;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.hardware.Camera.PreviewCallback;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
+import android.view.View.OnTouchListener;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+class Box extends View implements OnTouchListener {
+	int k;
+	float x1, y1, x2, y2;
+
+	public Box(Context context) {
+		super(context);
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		Paint paint = new Paint();
+		paint.setStyle(Style.STROKE);
+		paint.setStrokeWidth(5);
+		paint.setARGB(255, 255, 0, 0);
+		RectF rect = new RectF(x1, y1, x2, y2);
+		canvas.drawRect(rect, paint);
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		if(event.getPointerCount() > 1) {
+			x1 = event.getX(0);
+			y1 = event.getY(0);
+			x2 = event.getX(1);
+			y2 = event.getY(1);
+			for(int i=0; i<event.getPointerCount(); i++) {
+				if(x1 > event.getX(i))
+					x1 = event.getX(i);
+				if(y1 > event.getY(i))
+					y1 = event.getY(i);
+				if(x2 < event.getX(i))
+					x2 = event.getX(i);
+				if(y2 < event.getY(i))
+					y2 = event.getY(i);
+			}
+		}
+		return true;
+	}
+}
 
 public class AndroidVideo implements VideoInterface {
 
     private SurfaceView view;
     private Camera camera;
     private Video video;
+    private Box box;
     private boolean sent;
     private byte[] data;
     private Parameters parameters;
@@ -40,10 +91,13 @@ public class AndroidVideo implements VideoInterface {
         ViewGroup viewGroup = (ViewGroup) (container.getPlatformContainer());
         if (view == null) {
             view = new SurfaceView(viewGroup.getContext());
+            box = new Box(viewGroup.getContext());
 
-            //        int lHeight = LinearLayout.LayoutParams.FILL_PARENT;
-            //        int lWidth = LinearLayout.LayoutParams.FILL_PARENT;
-            viewGroup.addView(view);
+            int lHeight = LinearLayout.LayoutParams.FILL_PARENT;
+            int lWidth = LinearLayout.LayoutParams.FILL_PARENT;
+            viewGroup.addView(view, lHeight, lWidth);
+            viewGroup.addView(box);
+            view.setOnTouchListener(box);
             //        view.setBackgroundColor(Color.WHITE);
             view.getHolder().addCallback(new Callback() {
 
@@ -66,6 +120,7 @@ public class AndroidVideo implements VideoInterface {
                         parameters = camera.getParameters();
                         parameters.setPreviewSize(320, 240);
                         parameters.setPreviewFrameRate(5);
+                        parameters.setJpegQuality(25);
                         camera.setParameters(parameters);
                         camera.setPreviewDisplay(holder);
                         camera.startPreview();
@@ -76,6 +131,7 @@ public class AndroidVideo implements VideoInterface {
                             @Override
                             public void onPreviewFrame(byte[] data,
                                     Camera camera) {
+                        		box.invalidate();
                                 synchronized (AndroidVideo.this) {
                                     if (c % 5 < 3) {
                                         sent = false;
@@ -142,5 +198,13 @@ public class AndroidVideo implements VideoInterface {
         } catch (IllegalActionException e) {
             e.printStackTrace();
         }
+    }
+    
+    public void updateBoundingBox(float x1, float y1, float x2, float y2) {
+    	float ratio = (float)2.5;
+    	box.x1 = x1*ratio;
+    	box.x2 = x2*ratio;
+    	box.y1 = y1*ratio;
+    	box.y2 = y2*ratio;
     }
 }
